50a882fa810160c56abff3ef250d11cad8ec7187
chris-max-p <ichpaul@gmx.net>
2024-01-25 09:47:27 +0100

    use jest testing instead of karma (as karma is deprecated)

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -2,13 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
+import { MatIconModule } from '@angular/material/icon';
+
 describe('ConfirmDialogComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent],
+      imports: [
+        MatIconModule,
+      ]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
